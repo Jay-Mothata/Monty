e1e5c12e348c72841d85bf78222352aa777a409f
jay-mothata <joey.mothata@gmail.com>
2024-01-18 14:57:26 -0800

    Multiplies the second top element of the stack with the top element of the stack.

---
+++ mul.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * jay_mul - multiplies the top two elements of the stack.
+ * @jay_head: stack head
+ * @jay_counter: line_number
+ * Return: No return value
+ */
+
+void jay_mul(stack_t **jay_head, unsigned int jay_counter)
+{
+	stack_t *jay_h;
+	int jay_len = 0, jay_aux;
+
+	jay_h = *jay_head;
+	while (jay_h)
+	{
+		jay_h = jay_h->next;
+		jay_len++;
+	}
+	if (jay_len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", jay_counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*jay_head);
+		exit(EXIT_FAILURE);
+	}
+	jay_h = *jay_head;
+	jay_aux = jay_h->next->n * jay_h->n;
+	jay_h->next->n = jay_aux;
+	*jay_head = jay_h->next;
+	free(jay_h);
+}
+
