eec8ddecffd750894f14976578ce6ad829cd5ae8
jay-mothata <joey.mothata@gmail.com>
2024-01-18 14:51:46 -0800

    The opcode nop doesn’t do anything.

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * jay_nop - does nothing
+ * @jay_head: stack head
+ * @jay_counter: line number
+ * Return: No return value
+ */
+
+void jay_nop(stack_t **jay_head, unsigned int jay_counter)
+{
+	(void)jay_counter;
+	(void)jay_head;
+}
