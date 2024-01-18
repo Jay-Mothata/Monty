126aedb5dc5f9b2fc7de3fd88618cd14140d5071
jay-mothata <joey.mothata@gmail.com>
2024-01-18 15:01:46 -0800

    Sets the format of the data to a stack (LIFO).

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * jay_stack - prints the top
+ * @jay_head: stack head
+ * @jay_counter: line_number
+ * Return: no return
+ */
+
+void jay_stack(stack_t **jay_head, unsigned int jay_counter)
+{
+	(void)jay_head;
+	(void)jay_counter;
+	bus.lifi = 0;
+}
